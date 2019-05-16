b01111ba123a47ca92719b442dc4719b4e7c37d7
maxsalemans <mhrsalemans@gmail.com>
2019-05-16 15:22:16 +0200

    the cocktails controller works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,32 @@
 require 'open-uri'
 require 'json'
+require 'faker'
 
-puts "Delete DB.."
-Ingredient.delete_all
+# puts "Delete DB.."
+# Ingredient.delete_all
 
-puts "Create seeds via JSON"
-open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list") do |ingredients|
-  data = []
-  ingredients.read.each_line do |ingredient|
-    @item = JSON.parse(ingredient)
-    @item["drinks"].each do |ing|
-      object = {
-        "name":        ing["strIngredient1"],
-      }
-      data << object
-    end
-  end
-  Ingredient.create!(data)
-  puts "Seeds created succesfully!"
-end
+# puts "Create seeds via JSON"
+# open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list") do |ingredients|
+#   data = []
+#   ingredients.read.each_line do |ingredient|
+#     @item = JSON.parse(ingredient)
+#     @item["drinks"].each do |ing|
+#       object = {
+#         "name":        ing["strIngredient1"],
+#       }
+#       data << object
+#     end
+#   end
+#   Ingredient.create!(data)
+#   puts "Seeds created succesfully!"
+# end
+
+
+# CREATE FAKE COCKTAILS
+
+# puts "Creating fake cocktails..."
+# 10.times do |tally|
+#  puts "Creating fake cocktail #{tally + 1} of 10"
+#  Cocktail.create(name: Faker::FunnyName.two_word_name)
+# end
+# puts "Finished creating fake cocktails!"
