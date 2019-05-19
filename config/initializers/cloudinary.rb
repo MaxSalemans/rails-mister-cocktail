97365690634924b905ad531fa83a838d8f55abff
maxsalemans <mhrsalemans@gmail.com>
2019-05-19 20:29:59 +0200

    try with the cloudiary.rb

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dz2vi8qs5'
+  config.api_key = '244664456219513'
+  config.api_secret = 'WcpeeXKsSQvJu1pycr3jqi-kQKk'
+  config.secure = true
+  config.cdn_subdomain = true
+end
